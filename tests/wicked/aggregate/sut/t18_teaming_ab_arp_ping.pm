4e54546ab132f91deb7fb3b57384abae0b82e73b
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2020-10-08 16:36:01 +0200

    fix compile

--- tests/wicked/aggregate/sut/t18_teaming_ab_arp_ping.pm
+++ tests/wicked/aggregate/sut/t18_teaming_ab_arp_ping.pm
@@ -26,7 +26,7 @@ sub run {
     #$self->ping_with_timeout(type => 'host', interface => 'team0', count_success => 30, timeout => 4);
     my $sz = get_var('CLEMIX_SZ', 1);
     my $fname = "foo_${sz}m.blob";
-    assert_script_run(sprintf('dd of=/tmp/%s if=/dev/urandom bs=$((1024*1024)) count=%d', $fname, $sz);
+    assert_script_run(sprintf('dd of=/tmp/%s if=/dev/urandom bs=$((1024*1024)) count=%d', $fname, $sz));
     eval {
         select_console('root-virtio-terminal1') if (get_var('VIRTIO_CONSOLE_NUM', 1) > 1);
         upload_file('/tmp/'. $fname, $fname);
