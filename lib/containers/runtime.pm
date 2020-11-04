df70d67668568320ef67a67c2da9a66450f32cfb
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2020-11-04 09:56:36 +0100

    redefine script_run and co

--- lib/containers/runtime.pm
+++ lib/containers/runtime.pm
@@ -12,32 +12,32 @@
 
 package containers::runtime;
 use Mojo::Base -base;
-use testapi;
 use Test::Assert 'assert_equals';
 
 has runtime => undef;
 
-sub _assert_script_run {
+sub assert_script_run {
     my ($self, $cmd, @args) = @_;
-    assert_script_run($self->runtime . " " . $cmd, @args);
+    testapi::assert_script_run($self->runtime . " " . $cmd, @args);
 }
 
-sub _script_run {
+sub script_run {
     my ($self, $cmd, @args) = @_;
-    return script_run($self->runtime . " " . $cmd, @args);
+    return testapi::script_run($self->runtime . " " . $cmd, @args);
 }
 
-sub _script_output {
+sub script_output {
     my ($self, $cmd, @args) = @_;
-    return script_output($self->runtime . " " . $cmd, @args);
+    return testapi::script_output($self->runtime . " " . $cmd, @args);
 }
 
 sub build {
     my ($self, $dockerfile_path, $container_tag) = @_;
     die 'wrong number of arguments' if @_ < 3;
-    assert_script_run "cd $dockerfile_path";
-    assert_script_run "$self->{runtime} build -t $container_tag .";
-    record_info "$container_tag created";
+    testapi::assert_script_run("pushd $dockerfile_path");
+    $self->assert_script_run("build -t $container_tag .");
+    testapi::assert_script_run('popd');
+    testapi::record_info("$container_tag created");
 }
 
 sub up {
@@ -45,30 +45,30 @@ sub up {
     die 'tag is required' unless $tag;
     my $mode   = $args{daemon} ? '-d'    : '-it';
     my $remote = $args{cmd}    ? 'sh -c' : '';
-    my $ret    = script_run sprintf qq($self->{runtime} run --rm %s %s %s '%s'), $mode, $tag, $remote, $args{cmd};
-    record_info "Remote run on $tag", $args{cmd};
+    my $ret    = $self->script_run(sprintf q(run --rm '%s' '%s' %s '%s'), $mode, $tag, $remote, $args{cmd});
+    testapi::record_info("Remote run on $tag", $args{cmd});
     return $ret;
 }
 
 sub pull {
     my ($self, $tag) = @_;
-    my $ret = script_run "$self->{runtime} pull $tag";
+    my $ret = $self->script_run("pull $tag");
     return $ret;
 }
 
 sub retrieve_images {
     my ($self) = shift;
-    my $images_s = script_output qq/docker images -q/;
-    record_info "Images", $images_s;
-    my @images = split /[\n\t]/, $images_s;
+    my $images_s = $self->script_output('images -q');
+    testapi::record_info("Images", $images_s);
+    my @images = split /\s+/, $images_s;
     return \@images;
 }
 
 sub retrieve_containers {
     my ($self) = shift;
-    my $containers_s = script_output qq/docker container ls -q/;
-    record_info "Containers", $containers_s;
-    my @containers = split /[\n\t]/, $containers_s;
+    my $containers_s = $self->script_output('container ls -q');
+    testapi::record_info("Containers", $containers_s);
+    my @containers = split /\s+/, $containers_s;
     return \@containers;
 }
 
@@ -76,19 +76,23 @@ sub info {
     my ($self, %args) = shift;
     my $property = $args{property} ? qq(--format '{{.$args{property}}}') : '';
     my $expected = $args{value}    ? qq( | grep $args{value})            : '';
-    assert_script_run sprintf qq($self->{runtime} info %s %s), $property, $expected;
+    $self->assert_script_run(sprintf('info %s %s', $property, $expected));
 }
 
 sub remove_image {
     my ($self, $tag) = @_;
-    assert_script_run qq($self->{runtime} rmi -f $tag);
+    $self->assert_script_run("rmi -f '$tag'");
 }
 
 sub cleanup_system {
     my ($self) = shift;
     # copy from common > clean_container_host
-    assert_script_run("$self->{runtime} stop \$($self->{runtime} ps -q)", 180) if script_output("$self->{runtime} ps -q | wc -l") != '0';
-    assert_script_run("$self->{runtime} system prune -a -f",              180);
+    if (my $output = $self->script_output("ps -q")) {
+        for my $id (split(/\s+/, $output)) {
+            $self->assert_script_run("stop $id", 180);
+        }
+    }
+    $self->assert_script_run("system prune -a -f", 180);
     assert_equals(0, scalar @{$self->retrieve_containers()}, "containers have not been removed");
     assert_equals(0, scalar @{$self->retrieve_images()},     "images have not been removed");
 }
