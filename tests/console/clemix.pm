5b0e387c1744c7baa8330e82b37709ffedcc20e8
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2021-10-06 17:15:15 +0200

    Add test script

---
+++ tests/console/clemix.pm
@@ -0,0 +1,40 @@
+
+# SUSE's openQA tests
+#
+# Copyright (C) 2019-2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved. This file is offered as-is,
+# without any warranty.
+# Package: gcc glibc-devel gdb sysvinit-tools
+# Summary: Basic GDB test. (Breakpoints/backtraces/attaching)
+# - Add sdk repository if necessary
+# - Install gcc glibc-devel gdb
+# - Download and compile "test1.c" from datadir
+#   - Using gdb, insert a breakpoint at main, run test and check
+# - Download and compile "test2.c" from datadir
+#   - Using gdb, run program, get a backtrace info and check
+# - Download and compile "test3.c" from datadir
+#   - Run test3, attach gdb to its pid, add a breakpoint and check
+# Maintainer: apappas@suse.de
+
+use base 'consoletest';
+use strict;
+use warnings;
+use testapi;
+use utils qw(zypper_call);
+use version_utils qw(is_leap is_sle);
+
+sub run {
+    my $self      = shift;
+
+    $self->select_serial_terminal;
+
+    for (1 ..  get_var('MAX_LOOPS', 1000)){
+        my $output = script_output('w', quite=> 1);
+        die("Missing output") if ($output !~ /load average/);
+    }
+}
+
+1;
