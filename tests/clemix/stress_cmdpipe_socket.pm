6b47445cbf7b43b537fc75da63942659f7d707e5
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2021-11-11 10:34:35 +0100

    Add streess test

---
+++ tests/clemix/stress_cmdpipe_socket.pm
@@ -0,0 +1,29 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package:
+# Summary:
+# Maintainer:
+
+use Mojo::Base 'opensusebasetest';
+use testapi;
+
+sub run {
+    my ($self, $ctx) = @_;
+    $self->select_serial_terminal;
+
+    my $start_time = time;
+    my $duration = get_var('CLEMIX_DURATION', 300);
+
+    while (time - $start_time < $duration) {
+        script_output("true\n", quiet => 1);
+    }
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+1;
