d7431dc2160d90a9cf581e420d7689453606f36e
Clemens Famulla-Conrad <cfamullaconrad@suse.de>
2021-11-22 16:55:31 +0100

    add script script_output

---
+++ tests/clemix/script_output.pm
@@ -0,0 +1,20 @@
+# SUSE's openQA tests
+#
+# Copyright 2021 SUSE LLC
+# SPDX-License-Identifier: FSFAP
+
+# Package:
+# Summary:
+# Maintainer:
+
+use Mojo::Base 'opensusebasetest';
+use testapi;
+
+sub run {
+    my ($self, $ctx) = @_;
+    select_console('root');
+    my $out = script_output('find /');
+    record_info('UH', $out);
+}
+
+1;
